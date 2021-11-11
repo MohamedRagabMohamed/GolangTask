b9c3bd5557826dcf9d57089ab39cf0a2f7a86b6b
mohamed ragab <mohamedragab@techytypes.com>
2021-11-11 13:30:02 +0200

    create CRUD operation in mamory

--- apis.go
+++ apis.go
@@ -2,44 +2,27 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 )
 
 //TODO change all name to Users
 func Posts(g *gin.Context) {
 	//TODO if len is retrun 200
 	if len(Users) == 0 {
-		g.JSON(404, "Not found users")
+		g.JSON(http.StatusOK, "Not found users")
 	} else {
-		/*var outUsers []interface{}
-		for _, user := range Users {
-			outUsers = append(outUsers, user)
-		}*/
 		g.JSON(http.StatusOK, gin.H{
-			"users": Users[1],
+			"users": Users,
 		})
 	}
 }
 
 func Store(g *gin.Context) {
 	//TODO Search in gin validation and body request not query param
-	enteredName := g.Query("name")
-	enteredEmail := g.Query("email")
-	enteredPassword := g.Query("password")
-	enteredPhoneNumber := g.Query("phoneNumber")
-
-	user := User{
-		Name:        enteredName,
-		Email:       enteredEmail,
-		Password:    enteredPassword,
-		PhoneNumber: enteredPhoneNumber,
-	}
-
-	validate := validator.New()
-	err := validate.Struct(user)
-
+	var user User
+	err :=g.BindJSON(&user)
 	if err != nil {
 		g.JSON(http.StatusBadRequest, gin.H{
 			"message": err.Error(),
@@ -55,13 +38,49 @@ func Store(g *gin.Context) {
 }
 
 func Update(g *gin.Context) {
-
+	id,_:= strconv.Atoi(g.Param("id"))
+	var user User
+	g.BindJSON(&user)
+	if _,found :=Users[id];found {
+		Users[id]=User{
+			Name: user.Name,
+			Email: user.Email,
+			Password: user.Password,
+			PhoneNumber: user.PhoneNumber,
+		}
+		g.JSON(http.StatusOK,gin.H{
+			"id":Users[id],
+		})
+	}else{
+		g.JSON(http.StatusNotFound,gin.H{
+			"message":"No found user",
+		})
+	}	
 }
 
 func Delete(g *gin.Context) {
-
+	id,_:= strconv.Atoi(g.Param("id"))
+	if value,found :=Users[id];found {
+		delete(Users, id)
+		g.JSON(http.StatusOK,gin.H{
+			"Deleted user":value,
+		})
+	}else{
+		g.JSON(http.StatusNotFound,gin.H{
+			"message":"No found user",
+		})
+	}
 }
 
 func Show(g *gin.Context) {
-
+	id,_:= strconv.Atoi(g.Param("id"))
+	if value,found :=Users[id];found {
+		g.JSON(http.StatusOK,gin.H{
+			"id":value,
+		})
+	}else{
+		g.JSON(http.StatusNotFound,gin.H{
+			"message":"No found user",
+		})
+	}
 }
